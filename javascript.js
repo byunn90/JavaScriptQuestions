04e42ccd4716d21d3a2424bd0556dfa603e45ccf
byunn90 <kayhanturk90@Live.com>
2023-12-13 07:30:56 +1100

    loops

--- javascript.js
+++ javascript.js
@@ -122,3 +122,14 @@
 // Notes
 // For this question, output an array, not a set. These are two distinctly different data structures in JavaScript (although they can be converted from one to the other).
 // See resources for a hint if you get stuck.
+// Go over this Tommorrow
+// function set(arr) {
+//   const newArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!newArray.includes(arr[i])) {
+//       newArray.push(arr[i]);
+//     }
+//   }
+//   return newArray;
+// }
+// console.log(set([1, 1, 1, 2, 3, 4, 4, 5]));
