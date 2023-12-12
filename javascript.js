73972ffd039b47ecf7ab52c3a5c7c711a2f3e054
byunn90 <kayhanturk90@Live.com>
2023-12-12 20:29:49 +1100

    start

--- javascript.js
+++ javascript.js
@@ -37,22 +37,42 @@
 // Notes
 // Your output must be case-sensitive (see second example).
 
-function charCount(myChar, str) {
-  let z = 0;
-  for (let i = 0; i < str.length; i++) {
-    console.log(str[i]);
-    if (myChar === str[i]) {
-      ++z;
-    }
-  }
-  return z;
-}
-console.log(charCount("a", "edabit"));
-// Answer 1 ⬆
-console.log("___");
-console.log(charCount("c", "Chamber of secrets"));
-// Answer 1 ⬆
-console.log("___");
-console.log(charCount("b", "big fat bubble"));
-console.log("___");
+// function charCount(myChar, str) {
+//   let z = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+//     if (myChar === str[i]) {
+//       ++z;
+//     }
+//   }
+//   return z;
+// }
+// console.log(charCount("a", "edabit"));
+// // Answer 1 ⬆
+// console.log("___");
+// console.log(charCount("c", "Chamber of secrets"));
+// // Answer 1 ⬆
+// console.log("___");
+// console.log(charCount("b", "big fat bubble"));
+// console.log("___");
 // Answer 4 ⬆
+// Filter out Strings from an Array
+// Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+
+// Examples
+// filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+// filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+// filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+// Notes
+// Zero is a non-negative integer.
+// The given array only has integers and strings.
+// Numbers in the array should not repeat.
+// The original order must be maintained.
+function filterArray(arr) {
+  if (arr === "") {
+    return;
+  } else {
+    return arr;
+  }
+}
+console.log(filterArray([1, 2, 3, "b", "a"]));
