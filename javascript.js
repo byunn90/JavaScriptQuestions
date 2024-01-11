81a328834e1a9442071e78f9c9d31504bee51582
byunn90 <kayhanturk90@Live.com>
2024-01-11 16:58:20 +1100

    ,

--- javascript.js
+++ javascript.js
@@ -759,4 +759,29 @@
 // runningAthlete(["run", "run", "run", "run", "run"], "_|_|_") ➞ "_/_/_"
 // Notes
 // N/A
-function runningAthlete(act, txt) {}
+// Special Arrays
+// An array is special if every even index contains an even number and every odd index contains an odd number. Create a function that returns true if an array is special, and false otherwise.
+
+// Examples
+// isSpecialArray([2, 7, 4, 9, 6, 1, 6, 3]) ➞ true
+// // Even indices: [2, 4, 6, 6]; Odd indices: [7, 9, 1, 3]
+
+// isSpecialArray([2, 7, 9, 1, 6, 1, 6, 3]) ➞ false
+// // Index 2 has an odd number 9.
+
+// isSpecialArray([2, 7, 8, 8, 6, 1, 6, 3]) ➞ false
+// // Index 3 has an even number 8.
+// Notes
+// N/A
+// get odd numbers
+//
+function isSpecialArray(arr) {
+  // get odd numbers
+  //
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+  if (element % 2 === 0) {
+  }
+}
+isSpecialArray([1, 2, 3, 4, 5, 6, 7, 8]);
