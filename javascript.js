5b7982cc917b37db0f4675ae71b091e8d21cb4b2
byunn90 <kayhanturk90@Live.com>
2024-02-24 15:16:25 +1100

    cleaver split

--- javascript.js
+++ javascript.js
@@ -978,27 +978,45 @@
 // 2 <= n <= 1e12
 // Check for squares for numbers
 // check for prime numbers from sqaures first
-function mobius(n) {
-  let count = 0;
+// function mobius(n) {
+//   let count = 0;
 
-  for (let i = 2; i * i <= n; i++) {
-    if (n % (i * i) === 0) {
-      return 0;
-    }
-    if (n % i === 0) {
-      count++;
-      while (n % i === 0) {
-        n /= i;
-      }
-    }
-  }
-  if (n > 1) count++;
+//   for (let i = 2; i * i <= n; i++) {
+//     if (n % (i * i) === 0) {
+//       return 0;
+//     }
+//     if (n % i === 0) {
+//       count++;
+//       while (n % i === 0) {
+//         n /= i;
+//       }
+//     }
+//   }
+//   if (n > 1) count++;
 
-  if (count % 2 === 0) {
-    return 1;
-  } else {
-    return -1;
-  }
-}
+//   if (count % 2 === 0) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// }
 
-console.log(mobius(15));
+// console.log(mobius(15));
+
+
+
+DESCRIPTION:
+//  You have a string. The string consists of words. Before words can be an exclamation mark !. Also some words are marked as one set with square brackets []. You task is to split the string into separate words and sets.
+//  The set can't be contained in another set.
+
+// Input:
+//  String with words (separated by spaces), ! and [].
+
+// Output:
+//  Array with separated words and sets.
+
+// Examples:
+
+// ('Buy a !car [!red green !white] [cheap or !new]')  =>  ['Buy', 'a', '!car', '[!red green !white]', '[cheap or !new]']
+// ('!Learning !javascript is [a joy]')                =>  ['!Learning', '!javascript', 'is', '[a joy]']
+// ('[Cats and dogs] are !beautiful and [cute]')       =>  ['[Cats and dogs]', 'are', '!beautiful', 'and', '[cute]']
