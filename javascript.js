cbf74bebaa1f644ed787b891cba65c1b9bd7ef59
byunn90 <kayhanturk90@Live.com>
2024-03-05 21:50:07 +1100

    ,

--- javascript.js
+++ javascript.js
@@ -1235,16 +1235,58 @@ twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
 twoSum([3, 2, 4], 6) // returns [1, 2] or [2, 1]
 */
 // Go over this again Note
-function twoSum(numbers, target) {
-  let map = new Map();
-  for (let i = 0; i < numbers.length; i++) {
-    let complement = target - numbers[i];
-    if (map.has(complement)) {
-      return [map.get(complement), i];
-    }
-    map.set(numbers[i], i);
-  }
-  return [];
-}
+// function twoSum(numbers, target) {
+//   let map = new Map();
+//   console.log(map);
+//   for (let i = 0; i < numbers.length; i++) {
+//     let complement = target - numbers[i];
+//     console.log(map);
+//     if (map.has(complement)) {
+//       return [map.get(complement), i];
+//     }
+//     map.set(numbers[i], i);
+//   }
+//   return [];
+// }
 
-console.log(twoSum([1, 2, 3, 4], 3));
+// console.log(twoSum([1, 2, 3, 4], 3));
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+console.log(points("5:2, 2:2, 1:1, 3:3"));
+
+function points(games) {
+  const gameScores = games.split(", ");
+  let totalPoints = 0;
+
+  for (let game of gameScores) {
+    const scores = game.split(":");
+    const x = parseInt(scores[0], 10);
+    const y = parseInt(scores[1], 10);
+
+    if (x > y) {
+      totalPoints += 3;
+    } else if (x === y) {
+      totalPoints += 1;
+    }
+  }
+
+  return totalPoints;
+}
+console.log(points("5:2, 2:2, 1:1, 3:3"));
