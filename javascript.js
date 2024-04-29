b410609e010baccb124f613f441f3b02bace8913
byunn90 <kayhanturk90@Live.com>
2024-04-29 17:33:39 +1000

    questions again

--- javascript.js
+++ javascript.js
@@ -2326,12 +2326,49 @@ Happy judging!
 // Hamburgers: 3 points
 
 // Hotdogs: 2 points
-const eatingComp = [
-  {
-    ChickenWings: 5,
-    HamBurgers: 3,
-    HotDogs: 2,
-  },
-];
-function scoreboard(whoAteWhat) {}
-scoreboard();
+// const eatingComp = [
+//   {
+//     ChickenWings: 5,
+//     HamBurgers: 3,
+//     HotDogs: 2,
+//   },
+// ];
+// function scoreboard(whoAteWhat) {}
+// scoreboard();
+/*
+Return a sorted list of objects
+973190% of 4993,774 of 5,083NotBobTheBuilder
+JavaScript
+TRAIN AGAINNEXT KATA
+Details
+Solutions
+Forks (2)
+Discourse (31)
+Collect|
+DESCRIPTION:
+You'll be passed an array of objects (list) - you must sort them in descending order based on the value of the specified property (sortBy).
+
+Example
+When sorted by "a", this:
+
+[
+  {"a": 1, "b": 3},
+  {"a": 3, "b": 2},
+  {"a": 2, "b": 40},
+  {"a": 4, "b": 12}
+]
+should return:
+
+[
+  {"a": 4, "b": 12},
+  {"a": 3, "b": 2},
+  {"a": 2, "b": 40},
+  {"a": 1, "b": 3}
+]
+The values will always be numbers, and the properties will always exist.
+
+
+*/
+function sortList(sortBy, list) {
+  return; //sorted list
+}
