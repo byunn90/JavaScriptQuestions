a693fcacde8fc104bb544f0a84421044112ca3bd
byunn90 <kayhanturk90@Live.com>
2024-07-04 16:10:49 +1000

    questions

--- javascript.js
+++ javascript.js
@@ -2396,15 +2396,75 @@ Return an array of the resulting objects.
 
 All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
 */
-function numObj(s) {
-  let result = [];
+// function numObj(s) {
+//   let result = [];
 
-  for (let num of s) {
-    let obj = {};
-    obj[num.toString()] = String.fromCharCode(num);
-    result.push(obj);
-    console.log(result);
-  }
-}
+//   for (let num of s) {
+//     let obj = {};
+//     obj[num.toString()] = String.fromCharCode(num);
+//     result.push(obj);
+//     console.log(result);
+//   }
+// }
 
-numObj([118, 117, 120, 13011111]);
+// numObj([118, 117, 120, 13011111]);
+// Object Questions 1
+/*
+Find the force of gravity between two objects
+Your job is to find the gravitational force between two spherical objects (obj1 , obj2).
+
+input
+Two arrays are given :
+
+arr_val (value array), consists of 3 elements
+1st element : mass of obj 1
+2nd element : mass of obj 2
+3rd element : distance between their centers
+arr_unit (unit array), consists of 3 elements
+1st element : unit for mass of obj 1
+2nd element : unit for mass of obj 2
+3rd element : unit for distance between their centers
+mass units are :
+
+kilogram (kg)
+gram (g)
+milligram (mg)
+microgram (μg)
+pound (lb)
+distance units are :
+
+meter (m)
+centimeter (cm)
+millimeter (mm)
+micrometer (μm)
+feet (ft)
+Note
+value of G = 6.67 × 10−11 N⋅kg−2⋅m2
+
+1 ft = 0.3048 m
+
+1 lb = 0.453592 kg
+
+return value must be Newton for force (obviously)
+
+μ copy this from here to use it in your solution
+
+*/
+// solution = (arr_val, arr_unit) => {
+//   // you code goes here
+// };
+// Drink Sorting
+// You will be given an array of drinks, with each drink being an object with two properties: name and price. Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+// Assume that the following array of drink objects needs to be sorted:
+
+// drinks = [
+//   {name: "lemonade", price: 50},
+//   {name: "lime", price: 10}
+// ]
+// The output of the sorted drinks object will be:
+
+// Examples
+// sortDrinkByPrice(drinks) ➞ [{name: "lime", price: 10}, {name: "lemonade", price: 50}]
+
+function sortDrinkByPrice(drinks) {}
